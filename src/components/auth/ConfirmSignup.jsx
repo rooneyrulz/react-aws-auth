4f4b2d188320ffa6e89af94ec04da0fadbe2a74a
ruzny <ruzny@ruzny.com>
2021-02-05 10:55:10 +0530

    setup formik for confirm email form & setup create-account component

--- src/components/auth/ConfirmSignup.jsx
+++ src/components/auth/ConfirmSignup.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const ConfirmSignup = () => {
+  const initialValues = {
+    username: '',
+    code: '',
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username must not be empty!'),
+    code: Yup.number()
+      .integer()
+      .required('Code must not be empty!'),
+  });
+
+  const onSubmit = (value) => console.log(value);
+
   return (
     <div className='confirm__sign__up flex flex-col justify-center items-center min-h-screen'>
       <div className='container mx-auto px-4 space-y-6 w-1/2'>
@@ -9,47 +25,83 @@ const ConfirmSignup = () => {
           Confirm Your Email
         </h1>
         <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col'>
-          <form>
-            <div className='mb-4'>
-              <label
-                className='block text-grey-darker text-sm font-bold mb-2'
-                for='username'
-              >
-                Username
-              </label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker'
-                id='username'
-                type='text'
-                placeholder='Enter your username'
-              />
-            </div>
-            <div className='mb-6'>
-              <label
-                className='block text-grey-darker text-sm font-bold mb-2'
-                for='code'
-              >
-                Confirmation Code
-              </label>
-              <input
-                className='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'
-                id='code'
-                type='number'
-                placeholder='Enter your code'
-              />
-            </div>
-            <div className='flex items-center justify-between'>
-              <button
-                className='bg-blue-600 text-white py-3 px-8 rounded'
-                type='button'
-              >
-                CONFIRM
-              </button>
-              <Link className='text-sm text-blue-500' to='/sign-up'>
-                Sign Up again?
-              </Link>
-            </div>
-          </form>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <div className='mb-4'>
+                <label
+                  className='block text-grey-darker text-sm font-bold mb-2'
+                  htmlFor='username'
+                >
+                  Username *
+                </label>
+                <Field name='username'>
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <input
+                        id='username'
+                        type='username'
+                        className={`shadow appearance-none outline-none border-2 border-transparent rounded w-full py-2 px-3 text-grey-darker  ${
+                          meta.touched && meta.error ? 'border-red-600 ' : null
+                        }`}
+                        placeholder='Enter your username'
+                        {...field}
+                      />
+                    );
+                  }}
+                </Field>
+                <ErrorMessage
+                  name='username'
+                  component='div'
+                  className='text-xs text-red-600 font-bold'
+                />
+              </div>
+              <div className='mb-6'>
+                <label
+                  className='block text-grey-darker text-sm font-bold mb-2'
+                  htmlFor='code'
+                >
+                  Confirmation Code *
+                </label>
+                <Field name='code'>
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <input
+                        id='code'
+                        type='number'
+                        className={`shadow appearance-none outline-none border-2 border-transparent rounded w-full py-2 px-3 text-grey-darker  ${
+                          meta.touched && meta.error ? 'border-red-600 ' : null
+                        }`}
+                        placeholder='Enter your code'
+                        {...field}
+                      />
+                    );
+                  }}
+                </Field>
+                <ErrorMessage
+                  name='code'
+                  component='div'
+                  className='text-xs text-red-600 font-bold'
+                />
+              </div>
+              <div className='flex items-center justify-between'>
+                <button
+                  className='bg-blue-600 text-white py-3 px-8 rounded'
+                  type='submit'
+                >
+                  CONFIRM
+                </button>
+                <Link className='text-sm text-blue-500' to='/sign-up'>
+                  Sign Up again?
+                </Link>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
